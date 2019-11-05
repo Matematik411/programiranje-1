dfd911fbdae5e44572720d6e305dd246f16f31a9
Nejc Zajc <nz3928@student.uni-lj.si>
2019-11-05 22:41:17 +0100

    Nalogice....

--- 04-uvod-v-ocaml/vaje/Vadim.ml
+++ 04-uvod-v-ocaml/vaje/Vadim.ml
@@ -1,3 +1,5 @@
+(* --------------- Working with lists -----------------*)
+
 (* 7. Flatten a nested list structure. (medium) *)
 
 type 'a node =
@@ -64,3 +66,84 @@ let slice list i k =
         | x :: xs -> slice' i k acc (j + 1) xs
     in
     slice' i k [] 0 list
+
+
+(* ------------------ ARITHMETIC -------------------------------------- *)
+
+(* 31. Determine whether a given integer number is prime. (medium) *)
+(* I'm working with positive integers here! *)
+
+let is_prime n = 
+    let rec is_not_divisor n d = 
+        d * d > n || (n mod d <> 0 && is_not_divisor n (d + 1))
+    in
+    n > 1 && is_not_divisor n 2
+
+(* 32. Determine the greatest common divisor of two positive integer numbers. (medium) *)
+
+let rec gcd m n =
+    let o = m mod n
+    in
+    if o = 0 then n
+    else gcd n o
+
+(* 33. Determine whether two positive integer numbers are coprime. (easy) *)
+
+let coprime m n = gcd m n = 1
+
+(* 34. Calculate Euler's totient function φ(m). (medium) *)
+
+let phi n = 
+    let rec phi' n k acc =
+        if n = 1 then 1
+        else
+        if n = k then acc
+        else
+        if coprime n k then phi' n (k + 1) (acc + 1)
+        else
+        phi' n (k + 1) acc
+    in
+    phi' n 1 0
+
+(* 35. Determine the prime factors of a given positive integer. (medium) *)
+
+let factors_list n = 
+    if n = 1 then []
+    else
+    let rec factors_list' n k acc = 
+        if n = 1 then List.rev acc
+        else
+        if n mod k = 0 then factors_list' (n / k) k (k :: acc)
+        else
+        factors_list' n (k + 1) acc
+    in
+    factors_list' n 2 []
+
+(* 36. Determine the prime factors of a given positive integer (2). (medium) *)
+
+let factors_pairs n = 
+    let rec factors_pairs' acc current = function
+        | [] -> [] (*doesn't happen *)
+        | x :: [] -> (x, (current + 1)) :: acc
+        | x :: y :: xs when x = y -> factors_pairs' acc (current + 1) (y :: xs)
+        | x :: y :: xs -> factors_pairs' ((x, (current + 1)):: acc) 0 (y :: xs)
+    in
+    List.rev (factors_pairs' [] 0 (factors_list n))
+
+(* 37. Calculate Euler's totient function φ(m) (improved). (medium) *)
+
+let pow n k =
+    let rec pow' n k acc = 
+        if k = 0 then acc
+        else pow' n (k - 1) (n * acc)
+    in
+    pow' n k 1
+
+
+let phi_improved n = 
+    let rec phi_improved' acc = function
+        | [] -> acc
+        | (x, k) :: xs -> phi_improved' (acc * (x - 1) * (pow x (k - 1))) xs
+    in
+    phi_improved' 1 (factors_pairs n)
+
