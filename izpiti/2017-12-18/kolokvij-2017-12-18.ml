f4933a21c0e7e16c9e713eb4c364bd67642fc985
Nejc Zajc <nz3928@student.uni-lj.si>
2019-11-14 22:45:30 +0100

    rešena 2 kolokvija... tista z rožnimi drevesi

--- izpiti/2017-12-18/kolokvij-2017-12-18.ml
+++ izpiti/2017-12-18/kolokvij-2017-12-18.ml
@@ -4,27 +4,38 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame tri cela števila ter vrne njihov produkt.
    Primer: /zmnozi 2 3 4 = 24/ *)
- let zmnozi = failwith "dopolni me"
+let zmnozi a b c = a * b * c 
 
 (* 1.2) Definirajte funkcijo, ki vzame celo število x in celo število k, ter
    vrne vrednost izraza x^3 + k.
    Primer: /afin_kub 2 1 = 9/ *)
- let afin_kub = failwith "dopolni me"
+let afin_kub x k = x * x * x + k
 
 (* 1.3) Definirajte funkcijo, ki vzame seznam in izračuna seznam vrednosti funkcije
    f(x) = x^3 + 2 za elemente vhodnega seznama.
    Primer: /vse_kubiraj_in_pristej_dva [1; 2; 3] = [3; 10; 29]/ *)
- let vse_kubiraj_in_pristej_dva = failwith "dopolni me"
+let vse_kubiraj_in_pristej_dva = 
+  let rec vse_kubiraj_in_pristej_dva' acc = function
+    | [] -> List.rev acc
+    | x :: xs -> vse_kubiraj_in_pristej_dva' ((afin_kub x 2) :: acc) xs
+  in
+  vse_kubiraj_in_pristej_dva' [] 
 
 (* 1.4) Definirajte funkcijo, ki varno vrne zadnji element seznama v primeru,
    da seznam ni prazen. Uporabite tip option.
    Primer: /zadnji_element [1; 2; 3] = Some 3/ *)
- let zadnji_element = failwith "dopolni me"
+let zadnji_element list = 
+  match (List.rev list) with
+  | [] -> None
+  | x :: xs -> Some x
 
 (* 1.5) Definirajte funkcijo, ki izračuna n-to Fibonaccijevo število.
    Pri tem upoštevamo začetna pogoja /fibonacci 0 = 1/ in /fibonacci 1 = 1/.
    Primer: /fibonacci 20 = 10946/ *)
- let fibonacci = failwith "dopolni me"
+let fibonacci n = 
+  let rec fibonacci' a b n = if n > 0 then fibonacci' b (a + b) (n - 1) else a
+  in
+  fibonacci' 1 1 n
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
@@ -35,7 +46,7 @@
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
+type 'a drevo = Roza of 'a * 'a drevo list
 
 (* 2.2) Definirajte naslednja rožna drevesa:
 
@@ -46,24 +57,47 @@ type 'a drevo = DopolniMe
 
  *)
 
-let t = failwith "dopolni me"
-let t' = failwith "dopolni me"
-let t'' = failwith "dopolni me"
+let t = Roza (1,[])
+let t' = Roza (2,[t;t;t;t;t;t])
+let t'' = Roza (3,[Roza(-1,[]); t'; Roza(0,[])])
 
 (* 2.3) Definirajte funkcijo, ki preveri ali je dano rožno drevo list drevesa,
    torej ima prazen gozd poddreves. *)
-let je_list = failwith "dopolni me"
+let je_list (Roza (_, gozd)) = gozd = []
 
 (* 2.4) Definirajte funkcijo, ki preveri, ali drevo celih števil vsebuje zgolj pozitivna števila. *)
-let vsa_pozitivna = failwith "dopolni me"
+let rec stakni acc = function
+  | [] -> acc
+  | x :: xs -> stakni (x :: acc) xs
+
+let vsa_pozitivna drevo = 
+  let rec vsa_pozitivna' (trenuten, rest) = 
+    match (trenuten, rest) with
+      | (Roza (a, []), []) -> a > 0
+      | (Roza (a, []), x :: xs) -> if a > 0 then vsa_pozitivna' (x, xs) else false
+      | (Roza (a, x :: xs), _) -> if a > 0 then vsa_pozitivna' (x, (stakni xs rest)) else false
+  in
+  vsa_pozitivna' (drevo, [])
+      
 
 (* 2.5) Definirajte funkcijo, ki izračuna največjo širino rožnega drevesa, torej največjo dolžino
    gozda, ki se pojavi v kateremkoli vozlišču rožnega drevesa. *)
-let sirina_drevesa = failwith "dopolni me"
+
+let rec sirina_drevesa (Roza(_, gozd)) = 
+  let rec sirina' acc = function
+    | Roza (_, []) -> max acc 1
+    | Roza (_, x :: xs) -> sirina' (max (sirina' 0 x) (acc + 1)) (Roza (0, xs))
+  in 
+  sirina' 0 (Roza(0, gozd))
 
 (* 2.6) Definirajte funkcijo, ki sestavi (poljubno) rožno drevo globine n.
    Vrednosti v korenih so poljubne. *)
-let globoko_drevo = failwith "dopolni me"
+let globoko_drevo = 
+  let rec gradi drevo = function
+    | 1 -> drevo
+    | n -> gradi (Roza (0,[drevo])) (n - 1)
+  in
+  gradi (Roza (0,[]))
 
 (* 2.7) Definirajte funkcijo, ki pretvori rožno drevo v seznam. Vrstni red vrednosti v seznamu
    pri tem ni pomemben.
@@ -74,4 +108,11 @@ let globoko_drevo = failwith "dopolni me"
    Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
    na primer List.map, niso repno rekurzivne, zato se jim raje
    izognite. *)
-let drevo_v_seznam = failwith "dopolni me"
+let drevo_v_seznam drevo = 
+  let rec drevo_v_seznam' (trenutno, rest) seznam =
+    match (trenutno, rest) with
+      | (Roza (a, []), []) -> a :: seznam
+      | (Roza (a, []), x :: xs) -> drevo_v_seznam' (x, xs) (a :: seznam)
+      | (Roza (a, x :: xs), _) -> drevo_v_seznam' (x, (stakni xs rest)) (a :: seznam)
+  in
+  drevo_v_seznam' (drevo, []) []
--- izpiti/2017-12-20/kolokvij-2017-12-20.ml
+++ izpiti/2017-12-20/kolokvij-2017-12-20.ml
@@ -4,29 +4,36 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame par in zamenja komponenti para.
    Primer: /obrni (2, 4) = (4, 2)/ *)
- let obrni = failwith "dopolni me"
+let obrni (a, b) = (b, a) 
 
 (* 1.2) Definirajte funkcijo, ki vzame par p in vrednost x in zamenja drugo
    komponento para p z x.
    Primer: /zamenjaj_drugo (2, 4) 7 = (2, 7)/ *)
- let zamenjaj_drugo = failwith "dopolni me"
+let zamenjaj_drugo (a, b) c = (a, c) 
 
 (* 1.3) Definirajte funkcijo, ki vzame seznam parov in izračuna nov seznam parov,
    ki imajo drugo komponento zamenjano z 42.
    Primer: /vsem_zamenjaj_drugo_z_42 [(12, 1); (2, 4)] = [(12, 42); (2, 42)]/ *)
- let vsem_zamenjaj_drugo_z_42 = failwith "dopolni me"
+let vsem_zamenjaj_drugo_z_42 =
+  let rec vsem_zamenjaj_drugo_z_42' acc = function
+    | [] -> List.rev acc
+    | x :: xs -> vsem_zamenjaj_drugo_z_42' ((zamenjaj_drugo x 42) :: acc) xs
+  in
+  vsem_zamenjaj_drugo_z_42' []
 
 (* 1.4) Definirajte funkcijo, ki varno vrne glavo seznama v primeru, ko seznam ni prazen.
    Uporabite tip option.
    Primer: /glava [1; 2; 3] = Some 1/ *)
- let glava = failwith "dopolni me"
-
+let glava = function
+  | [] -> None
+  | x :: _ -> Some x
+  
 (* 1.5) Definirajte funkcijo, vzame funkcijo (f: 'a -> 'b), neko vrednost (x : 'a) in
    celo število n. Funkcija naj vrne vrednost, ki jo dobimo če f n-krat uporabimo na x,
    torej f (f ... (f x)...).
    Primer: /uporabi_veckrat succ 0 420 = 420/ *)
- let uporabi_veckrat = failwith "dopolni me"
-
+let rec uporabi_veckrat f n x =
+  if n <= 0 then x else uporabi_veckrat f (n - 1) (f x)
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
 (* ======================================= *)
@@ -36,7 +43,7 @@
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
+type 'a drevo = Roza of 'a * 'a drevo list
 
 (* 2.2) Definirajte naslednja rožna drevesa:
 
@@ -47,25 +54,44 @@ type 'a drevo = DopolniMe
 
  *)
 
-let t = failwith "dopolni me"
-let t' = failwith "dopolni me"
-let t'' = failwith "dopolni me"
+let t = Roza (1,[])
+let t' = Roza (2,[t;t])
+let t'' = Roza (3,[Roza(-1,[]); t'; Roza(0,[])])
 
 (* 2.3) Definirajte funkcijo, ki vrne gozd rožnega drevesa. *)
-let vrni_gozd = failwith "dopolni me"
+let vrni_gozd (Roza (_, gozd)) = gozd
 
 (* 2.4) Definirajte funkcijo, ki izpiše vse vrednosti v rožnem drevesu celih števil.
    Števila naj bodo v ločenih vrsticah. Uporabite (print_int : int -> unit) in
    (print_newline : unit -> unit). *)
-let izpisi_vrednosti = failwith "dopolni me"
+let rec izpisi_vrednosti (Roza (a, gozd)) =
+  let rec izpisi_vrednosti' = function	(* ta za gozd *)
+    | [] -> ()
+    | x :: xs -> izpisi_vrednosti x; izpisi_vrednosti' xs
+  in
+  print_int a;
+  print_newline ();
+  izpisi_vrednosti' gozd
+
 
 (* 2.5) Definirajte funkcijo, ki izračuna globino rožnega drevesa, t.j. dolžino
    najdaljše poti od korena do lista. *)
-let globina = failwith "dopolni me"
+let rec globina (Roza(_, gozd)) = 
+  let rec globina' acc = function
+    | Roza (_, []) -> acc + 1
+    | Roza (_, x :: xs) -> globina' (max (globina' 0 x) acc) (Roza (0, xs))
+  in 
+  globina' 0 (Roza(0, gozd))
+  
 
 (* 2.6) Definirajte funkcijo, ki sestavi (poljubno) rožno drevo globine n.
    Vrednosti v korenih so poljubne. *)
-let globoko_drevo = failwith "dopolni me"
+let globoko_drevo = 
+  let rec gradi drevo = function
+    | 1 -> drevo
+    | n -> gradi (Roza (0,[drevo])) (n - 1)
+  in
+  gradi (Roza (0,[]))
 
 (* 2.7) Definirajte funkcijo, ki sprejme funkcijo (f : 'b -> 'a -> 'b) in začetno vrednost (acc : 'b)
    in funkcijo f zloži [fold] preko drevesa (t : 'a drevo). Vrstni red pri tem ni pomemben.
@@ -78,4 +104,16 @@ let globoko_drevo = failwith "dopolni me"
    Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
    na primer List.map, niso repno rekurzivne, zato se jim raje
    izognite. *)
-let zlozi = failwith "dopolni me"
+
+let rec stakni acc = function
+  | [] -> acc
+  | x :: xs -> stakni (x :: acc) xs
+
+let zlozi f acc drevo =
+  let rec zlozi' (trenutno, rest) value =
+    match (trenutno, rest) with
+      | (Roza (a, []), []) -> f value a
+      | (Roza (a, x :: xs), _) -> zlozi' (x, (stakni xs rest)) (f value a)
+      | (Roza (a, []), x :: xs) -> zlozi' (x, xs) (f value a)
+  in
+  zlozi' (drevo, []) acc
